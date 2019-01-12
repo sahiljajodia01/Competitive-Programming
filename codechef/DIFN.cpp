692e9118c14694a7015f98c5a88627302d2c446a
Sahil Jajodia <sahil.jajodia@gmail.com>
2019-01-12 19:20:40 +0530

    Changes to DIFN

--- codechef/DIFN.cpp
+++ codechef/DIFN.cpp
@@ -13,9 +13,40 @@ struct node {
 };
 
 
+bool areDistinct(vector< pair<string, int> > arr) {
+    int n = arr.size();
+    // Put all array elements in a map
+    map<int, bool> s ;
+    for(int i=0; i<n; i++){
+       s.insert(pair<int, bool>(arr[i].second,true));
+    }
+
+    // If all elements are distinct, size of
+    // map should be same array.
+    return (s.size() == arr.size());
+}
+
+int problematicNodes(vector< pair<string, int> > arr) {
+    int n = arr.size();
+    // Put all array elements in a map
+    map<int, int> s;
+    for(int i=0; i<n; i++){
+//        s.insert(pair<int, int>(arr[i].second,true));
+        s.find(arr[i].second) == s.end() ? (s[arr[i].second] = 1) : (s[arr[i].second]++);
+    }
+
+    for(map<int, int>::iterator it = s.begin(); it != s.end(); it++) {
+        if(it->second > 1) {
+            return it->first;
+        }
+    }
+}
+
+
+
 void diff_neighbours(node** arr, int N, int M) {
     vector<string> q;
-
+    // arr[0][0].data = 1;
     if(N == 1 && M == 1) {
         cout << 1 << "\n";
         cout << 1 << "\n";
@@ -23,13 +54,16 @@ void diff_neighbours(node** arr, int N, int M) {
     }
     else if(N == 1) {
         q.push_back("0 1");
+        // arr[0][1].data = 1;
     }
     else if(M == 1) {
         q.push_back("1 0");
+        // arr[1][0].data = 1;
     }
     else {
         q.push_back("0 1");
         q.push_back("1 0");
+        // arr[0][1].data = 1;
         arr[1][0].data = 2;
     }
 
@@ -43,7 +77,7 @@ void diff_neighbours(node** arr, int N, int M) {
         char token1 = *strtok(ar, " ");
         int row = token1 - '0';
         int row_copy = row;
-        cout << "Row: " << row << "\n"; 
+        cout << "Row: " << row << "\n";
         token1 = *strtok(NULL, " ");
         int col = token1 - '0';
         int col_copy = col;
@@ -52,7 +86,7 @@ void diff_neighbours(node** arr, int N, int M) {
         arr[row][col].visited = true;
 
         vector<string> nei;
-        
+
         for(int i = 0; i < 4; i++) {
             if(i == 3) {
                 row = row_copy;
@@ -79,16 +113,10 @@ void diff_neighbours(node** arr, int N, int M) {
             if((row >= 0 && row < N) && (col >= 0 && col < M) && (row != row_copy || col != col_copy)) {
                 string index = to_string(row) + " " + to_string(col);
                 cout << "Neighbour of " << ele << ": " << index << "\n";
-                // if(m.find(index) == m.end()) {
-                //     m[index] = arr[row][col].data;
-                //     nei.push_back(index);
-                // }
                 m.push_back(make_pair(index, arr[row][col].data));
             }
         }
 
-
-        // unordered_set<string, int>::iterator it = m.begin();
 
         vector<string> v;
         cout << "Hashmap: " << "\n";
@@ -98,52 +126,33 @@ void diff_neighbours(node** arr, int N, int M) {
             strcpy(ar1, m[it].first.c_str());
             token1 = *strtok(ar1, " ");
             row = token1 - '0';
-            // int row_copy = row;
-            // cout << "Row: " << row << "\n";
             token1 = *strtok(NULL, " ");
             col = token1 - '0';
-            // int col_copy = col;
 
-            // if(it == 0)
-            //     same = m[it].second;
-            // else {
-                // if(m[it].second == same || m[it].second == same1) {
-                    // for(int k = 0; k < nei.size(); k++) {
-                    //     if(m[nei[k]] != prev + 1) {
-                    //         arr[row][col] = prev + 1;
-                    //         prev = prev+1;
-                    //         break;
-                    //     }
-                    //     else {
-                    //         prev++;
-
-                    //     }
-                    // }
-            prev = 1;
-                    while(true) {
+            if(!areDistinct(m)) {
+                int conflictValue = problematicNodes(m);
+                prev = 1;
+                if(m[it].second == conflictValue) {
+                    while (true) {
                         int flag = 0;
-                        for(int it1 = 0; it1 < m.size(); it1++) {
+                        for (int it1 = 0; it1 < m.size(); it1++) {
+
                             int val = m[it1].second;
-                            if(prev+1 == val && m[it].first != m[it1].first) {
+                            if (prev + 1 == val && m[it].first != m[it1].first) {
                                 flag = 1;
                                 break;
                             }
                         }
-                        if(flag == 0) {
-                            arr[row][col].data = prev+1;
-                            m[it].second = prev+1;
+                        if (flag == 0) {
+                            arr[row][col].data = prev + 1;
+                            m[it].second = prev + 1;
                             break;
                         }
                         prev++;
                     }
-                // }
-                // else if(it == 1){
-                //     same1 = m[it].second;
-                // }
-                // else {
-                //     same = m[it].second;
-                // }
-            // }
+                }
+            }
+
 
             if(arr[row][col].visited == false) {
                 string index = to_string(row) + " " + to_string(col);
@@ -159,7 +168,7 @@ void diff_neighbours(node** arr, int N, int M) {
                 cout << arr[j][k].data << "\t";
             }
             cout << "\n";
-         }
+        }
 
         cout << "----------------------------------" << "\n";
         q.erase(q.begin());
@@ -171,7 +180,7 @@ void diff_neighbours(node** arr, int N, int M) {
             cout << arr[j][k].data << "\t";
         }
         cout << "\n";
-     }
+    }
 
 }
 
@@ -181,25 +190,25 @@ inline void scanint(int &x) {
     register int c = getchar_unlocked();
     x = 0;
     int neg = 0;
- 
+
     for(; ((c<48 || c>57) && c != '-'); c = getchar_unlocked());
- 
+
     if(c=='-') {
         neg = 1;
         c = getchar_unlocked();
     }
- 
+
     for(; c>47 && c<58 ; c = getchar_unlocked()) {
         x = (x<<1) + (x<<3) + c - 48;
     }
- 
+
     if(neg)
         x = -x;
 }
 
 int main() {
 
-	ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
 
@@ -207,15 +216,15 @@ int main() {
 
     for(int i = 0; i < t; i++) {
 
-		int N, M;
+        int N, M;
 
-		scanint(N);
-		scanint(M);
+        scanint(N);
+        scanint(M);
 
-		node** arr = new node*[N];
-		for(int j = 0; j < N; j++) {
-			arr[j] = new node[M];
-		}
+        node** arr = new node*[N];
+        for(int j = 0; j < N; j++) {
+            arr[j] = new node[M];
+        }
 
         // for(int j = 0; j < N; j++) {
         //     for(int k = 0; k < M; k++) {
@@ -223,10 +232,10 @@ int main() {
         //     }
         // }
 
-		diff_neighbours(arr, N, M);
-	}
+        diff_neighbours(arr, N, M);
+    }
 
 
 
-	return 0;
+    return 0;
 }
